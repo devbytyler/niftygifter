631fb6648e5cf6a37184022c599442b503a18fcc
tylerstephens814 <tylerstephens814@gmail.com>
2021-11-24 21:30:13 -0700

    Format time since

--- app/templatetags/gifty_filters.py
+++ app/templatetags/gifty_filters.py
@@ -5,4 +5,8 @@ register = template.Library()
 def url_target_blank(text):
     return text.replace('<a ', '<a target="_blank" ')
 
+def upto(value, delimiter=None):
+    return value.split(delimiter)[0]
+
 url_target_blank = register.filter(url_target_blank, is_safe = True)
+upto = register.filter(upto, is_safe = True)
