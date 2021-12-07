3da6fedf66cfc499aa9e5d62204ec94e9173611b
tylerstephens814 <tylerstephens814@gmail.com>
2021-12-06 21:11:10 -0700

    Fix bug where all notifications getting cleared

--- app/views.py
+++ app/views.py
@@ -117,7 +117,8 @@ def recipients_add_edit(request, event_id, recipient_id=None):
 def recipient_ideas(request, event_id, recipient_id):
     recipient = get_object_or_404(Recipient, pk=recipient_id)
     ideas = recipient.ideas.select_related("creator").order_by("-id")
-    Notification.objects.filter(content_type__model='idea', user=request.user).update(read=True)
+    ideas_ids = ideas.values_list('id', flat=True)
+    Notification.objects.filter(content_type__model='idea', object_id__in=ideas_ids, user=request.user).update(read=True)
 
     context = {"recipient": recipient, "ideas": ideas}
     return render(request, "app/recipient.html", context)
